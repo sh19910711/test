0be8d38005702564a8993ea454b2b0e6a30a4ce1
Hiroyuki Sano <sh19910711@gmail.com>
2014-10-24 17:46:29 +0900

    wip: vimrunner: add tests to run command on vim

---
+++ spec/vimrunner_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe "vimrunner" do
+
+  context ":echomsg 'hello'" do
+
+    subject { vim.command ":echomsg 'hello'" }
+    it { should eq "hello" }
+
+  end # :echomsg 'hello'
+
+  context ":echmsg 1+1" do
+
+    subject { vim.command ":echomsg 1+1" }
+    it { should eq "2" }
+
+  end
+
+end # vimrunner
