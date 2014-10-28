4dc4d3c5f3c39a1ab94088ff11318a679618146d
Hiroyuki Sano <sh19910711@gmail.com>
2014-10-28 12:14:56 +0900

    wip: remove colon from command (for Travis Vim)
    * https://travis-ci.org/social-snippet/vim-social-snippet/jobs/39224748#L271

--- spec/vimrunner_spec.rb
+++ spec/vimrunner_spec.rb
@@ -4,14 +4,14 @@ describe "vimrunner" do
 
   context ":echomsg 'hello'" do
 
-    subject { vim.command ":echomsg 'hello'" }
+    subject { vim.command "echomsg 'hello'" }
     it { should eq "hello" }
 
   end # :echomsg 'hello'
 
   context ":echmsg 1+1" do
 
-    subject { vim.command ":echomsg 1+1" }
+    subject { vim.command "echomsg 1+1" }
     it { should eq "2" }
 
   end
