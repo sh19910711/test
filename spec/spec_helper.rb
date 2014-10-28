99b091c5627a22cd963974330e7d5a65caeb087e
Hiroyuki Sano <sh19910711@gmail.com>
2014-10-28 14:04:29 +0900

    wip: vimrunner: load social_snippet.vim

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,11 @@ Vimrunner::RSpec.configure do |config|
 
   config.start_vim do
     vim = Vimrunner.start
+
+    # load social_snippet.vim
+    plugin_path = File.expand_path('../..', __FILE__)
+    vim.add_plugin(plugin_path, 'plugin/social_snippet.vim')
+
     vim
   end
 end
