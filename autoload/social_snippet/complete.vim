2dcb851a31fdd26a83e9b4df7955d72f6b5130c8
Hiroyuki Sano <sh19910711@gmail.com>
2014-11-04 18:42:41 +0900

    wip: complete: rename call() => repo_name()

--- autoload/social_snippet/complete.vim
+++ autoload/social_snippet/complete.vim
@@ -1,4 +1,4 @@
-function! social_snippet#complete#call(keyword)
+function! social_snippet#complete#repo_name(keyword)
   :let ret = split(system("sspm complete " . shellescape(a:keyword)), "\n")
   return ret
 endfunction
--- spec/complete_spec.rb
+++ spec/complete_spec.rb
@@ -2,11 +2,11 @@ require "spec_helper"
 
 describe "social_snippet#complete" do
 
-  describe "#call" do
+  describe "#repo_name" do
 
-    context "call(ex)" do
+    context "repo_name(ex)" do
 
-      before { vim.command "call append(0, join(social_snippet#complete#call('@snip <ex'), \"\n\"))" }
+      before { vim.command "call append(0, join(social_snippet#complete#repo_name('@snip <ex'), \"\n\"))" }
       let(:output) { vim_current_buffer }
       it { expect(output).to match /example-repo/ }
 
