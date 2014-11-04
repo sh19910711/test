aec23db036a825a5add915a0465bc364d7203e7d
Hiroyuki Sano <sh19910711@gmail.com>
2014-11-04 19:01:47 +0900

    wip: RSpec: add enable_social_snippet() as helper

--- spec/commands/insert_snippet_spec.rb
+++ spec/commands/insert_snippet_spec.rb
@@ -2,59 +2,65 @@ require "spec_helper"
 
 describe :SocialSnippetInsertSnippet do
 
-  context "without snip tags" do
+  context "enable social_snippet" do
 
-    before do
-      vim.insert [
-        "hello",
-      ].join("\n")
-    end
+    before { enable_social_snippet }
 
-    context "call :SocialSnippetInsertSnippet" do
+    context "without snip tags" do
 
       before do
-        vim.command "SocialSnippetInsertSnippet"
+        vim.insert [
+          "hello",
+        ].join("\n")
       end
 
-      let(:output) { vim_current_buffer }
-      it { expect(output).to match /hello/ }
+      context "call :SocialSnippetInsertSnippet" do
 
-    end # call :SocialSnippetInsertSnippet
+        before do
+          vim.command "SocialSnippetInsertSnippet"
+        end
 
-  end # without snip tags
+        let(:output) { vim_current_buffer }
+        it { expect(output).to match /hello/ }
 
-  context "with a snip tags" do
+      end # call :SocialSnippetInsertSnippet
 
-    before do
-      vim.insert [
-        "#include <iostream>",
-        "",
-        "// @snip <example-repo:func.cpp>",
-        "",
-        "int main() {",
-        '  std::cout << "func() => " << func() << std::endl;',
-        "  return 0;",
-        "}",
-      ].join("\n")
-    end
+    end # without snip tags
 
-    context "call :SocialSnippetInsertSnippet" do
+    context "with a snip tags" do
 
       before do
-        vim.command :SocialSnippetInsertSnippet
+        vim.insert [
+          "#include <iostream>",
+          "",
+          "// @snip <example-repo:func.cpp>",
+          "",
+          "int main() {",
+          '  std::cout << "func() => " << func() << std::endl;',
+          "  return 0;",
+          "}",
+        ].join("\n")
       end
 
-      let(:output) { vim_current_buffer }
-      it { expect(output).to match /include <iostream>/ }
-      it { expect(output).to_not match /\/\/ @snip / }
-      it { expect(output).to match /\/\/ @snippet / }
-      it { expect(output).to match /<example-repo#\d+\.\d+\.\d+:func.cpp>/ }
-      it { expect(output).to match /<example-repo#\d+\.\d+\.\d+:func\/sub_func_1.cpp>/ }
-      it { expect(output).to match /<example-repo#\d+\.\d+\.\d+:func\/sub_func_2.cpp>/ }
-      it { expect(output).to match /<example-repo#\d+\.\d+\.\d+:func\/sub_func_3.cpp>/ }
+      context "call :SocialSnippetInsertSnippet" do
 
-    end # call :SocialSnippetInsertSnippet
+        before do
+          vim.command :SocialSnippetInsertSnippet
+        end
 
-  end # with a snip tag
+        let(:output) { vim_current_buffer }
+        it { expect(output).to match /include <iostream>/ }
+        it { expect(output).to_not match /\/\/ @snip / }
+        it { expect(output).to match /\/\/ @snippet / }
+        it { expect(output).to match /<example-repo#\d+\.\d+\.\d+:func.cpp>/ }
+        it { expect(output).to match /<example-repo#\d+\.\d+\.\d+:func\/sub_func_1.cpp>/ }
+        it { expect(output).to match /<example-repo#\d+\.\d+\.\d+:func\/sub_func_2.cpp>/ }
+        it { expect(output).to match /<example-repo#\d+\.\d+\.\d+:func\/sub_func_3.cpp>/ }
+
+      end # call :SocialSnippetInsertSnippet
+
+    end # with a snip tag
+
+  end # enable social_snippet
 
 end # vimrunner
--- spec/complete_spec.rb
+++ spec/complete_spec.rb
@@ -2,51 +2,57 @@ require "spec_helper"
 
 describe "social_snippet#complete" do
 
-  describe "#is_repo_name_mode" do
+  context "enable social_snippet" do
 
-    context "str = // @snip" do
-      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip")' }
-      it { should eq "0" }
-    end
+    before { enable_social_snippet }
 
-    context "str = // @snip <" do
-      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <")' }
-      it { should eq "1" }
-    end
+    describe "#is_repo_name_mode" do
 
-    context "str = // @snip <repo" do
-      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo")' }
-      it { should eq "1" }
-    end
+      context "str = // @snip" do
+        subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip")' }
+        it { should eq "0" }
+      end
 
-    context "str = // @snip <repo#1.2.3" do
-      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo#1.2.3")' }
-      it { should eq "0" }
-    end
+      context "str = // @snip <" do
+        subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <")' }
+        it { should eq "1" }
+      end
 
-    context "str = // @snip <repo:path/to/file" do
-      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo:path/to/file")' }
-      it { should eq "0" }
-    end
+      context "str = // @snip <repo" do
+        subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo")' }
+        it { should eq "1" }
+      end
 
-    context "str = // @snip <repo#1.2.3:path/to/file" do
-      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo#1.2.3:path/to/file")' }
-      it { should eq "0" }
-    end
+      context "str = // @snip <repo#1.2.3" do
+        subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo#1.2.3")' }
+        it { should eq "0" }
+      end
 
-  end
+      context "str = // @snip <repo:path/to/file" do
+        subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo:path/to/file")' }
+        it { should eq "0" }
+      end
 
-  describe "#repo_name" do
+      context "str = // @snip <repo#1.2.3:path/to/file" do
+        subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo#1.2.3:path/to/file")' }
+        it { should eq "0" }
+      end
 
-    context "keyword = ex" do
+    end # is_repo_name_mode
 
-      before { vim.command "call append(0, join(social_snippet#complete#repo_name('@snip <ex'), \"\n\"))" }
-      let(:output) { vim_current_buffer }
-      it { expect(output).to match /example-repo/ }
+    describe "#repo_name" do
 
-    end
+      context "keyword = ex" do
 
-  end
+        before { vim.command "call append(0, join(social_snippet#complete#repo_name('@snip <ex'), \"\n\"))" }
+        let(:output) { vim_current_buffer }
+        it { expect(output).to match /example-repo/ }
 
-end
+      end
+
+    end # repo_name
+
+  end # enable social_snippet
+
+end # social_snippet#complete
 
