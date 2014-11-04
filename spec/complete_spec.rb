1895ce49be6ce88bfa453c6abe1d217ee720b06a
Hiroyuki Sano <sh19910711@gmail.com>
2014-11-04 18:58:47 +0900

    wip: complete: spec: add tests for is_repo_name_mode()

--- spec/complete_spec.rb
+++ spec/complete_spec.rb
@@ -2,9 +2,43 @@ require "spec_helper"
 
 describe "social_snippet#complete" do
 
+  describe "#is_repo_name_mode" do
+
+    context "str = // @snip" do
+      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip")' }
+      it { should eq "0" }
+    end
+
+    context "str = // @snip <" do
+      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <")' }
+      it { should eq "1" }
+    end
+
+    context "str = // @snip <repo" do
+      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo")' }
+      it { should eq "1" }
+    end
+
+    context "str = // @snip <repo#1.2.3" do
+      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo#1.2.3")' }
+      it { should eq "0" }
+    end
+
+    context "str = // @snip <repo:path/to/file" do
+      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo:path/to/file")' }
+      it { should eq "0" }
+    end
+
+    context "str = // @snip <repo#1.2.3:path/to/file" do
+      subject { vim.command 'echomsg social_snippet#complete#is_repo_name_mode("// @snip <repo#1.2.3:path/to/file")' }
+      it { should eq "0" }
+    end
+
+  end
+
   describe "#repo_name" do
 
-    context "repo_name(ex)" do
+    context "keyword = ex" do
 
       before { vim.command "call append(0, join(social_snippet#complete#repo_name('@snip <ex'), \"\n\"))" }
       let(:output) { vim_current_buffer }
