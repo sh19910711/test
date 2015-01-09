2cc4a02e66faa48938834f89b81057d2f483bf8b
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-09 13:05:07 +0900

    wip

--- lib/git/contest/git.rb
+++ lib/git/contest/git.rb
@@ -14,6 +14,7 @@ module Git
 
   # use return value
   def self.do_no_echo(*args)
+    puts "git #{args.join(' ')}" # DEBUG
     puts "git #{args.join(' ')}" if ENV['GIT_CONTEST_DEBUG'] == 'ON'
     system "git #{args.join(' ')} >/dev/null 2>&1"
   end
