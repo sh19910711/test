44c725f84fcb73bc625186b81d3ad3e147444e08
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-11 13:07:52 +0900

    wip

--- codeforces/codeforces.go
+++ codeforces/codeforces.go
@@ -1,5 +1,54 @@
 package codeforces
 
+import "log"
+import "net/http"
+import "io"
+import "net/url"
+import "encoding/json"
+
+const ENDPOINT = "http://codeforces.com/api"
+
+type Client struct {
+  client *http.Client
+}
+
+type Contest struct {
+  Id int `json:"id"`
+  Name string `json:"name"`
+}
+
+func api(path string) *url.URL {
+  res, _ := url.Parse(ENDPOINT + path)
+  return res
+}
+
+type ResultContest struct {
+  Status string `json:"status"`
+  Contests []Contest `json:"result"`
+}
+
+func result(r io.Reader) []Contest {
+  ret := new(ResultContest)
+  decoder := json.NewDecoder(r)
+  err := decoder.Decode(&ret)
+  if err != nil {
+    log.Fatalf("Error on parsing result")
+  }
+  if ret.Status != "OK" {
+    log.Fatalf("Error on calling web api: %s", ret.Status)
+  }
+  return ret.Contests
+}
+
+func ContestList() []Contest {
+  req, err := http.Get(api("/contest.list").String())
+  if err != nil {
+    log.Fatalf("Error on HTTP request")
+  }
+  defer req.Body.Close()
+  return result(req.Body)
+}
+
 func Hello() string {
   return "Hello"
 }
--- codeforces/codeforces_test.go
+++ codeforces/codeforces_test.go
@@ -9,3 +9,24 @@ func TestHello(t *testing.T) {
   }
 }
 
+func TestContestList(t *testing.T) {
+  ret := ContestList()
+  if len(ret) != 501 {
+    t.Fail()
+  }
+}
+
+func TestApi(t *testing.T) {
+  ret := api("/contest.list").String()
+  if ret != ENDPOINT + "/contest.list" {
+    t.Fail()
+  }
+}
+
+func TestApiWithQuery(t *testing.T) {
+  ret := api("/contest.list?q=foo").String()
+  if ret != ENDPOINT + "/contest.list?q=foo" {
+    t.Fail()
+  }
+}
+
