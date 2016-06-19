1549a969ea41a39547db722402f958ce36773ec1
Hiroyuki Sano <sh19910711@gmail.com>
2016-06-19 16:23:56 +0900

    wip

--- script/convert.bash
+++ script/convert.bash
@@ -4,15 +4,21 @@
 convert_all() {
   local gitroot=$(git rev-parse --show-cdup)
   cd ${gitroot:-$PWD}
+
   git submodule | while read line; do
-    local commit=$(echo $line | awk '{print $1}')
+    local commit_id=$(echo $line | awk '{print $1}')
     local module=$(echo $line | awk '{print $2}')
     local path=$(git config -f .gitmodules submodule.$module.path)
     local url=$(git config -f .gitmodules submodule.$module.url)
-    if [[ -f ./$path/.git ]] || [ ! -d ./$path ]; then
+
+    if [[ -f ./$path/.git ]] || [[ ! -e ./$path/.git ]]; then
       echo convert: $module
       rm -rf ./$path
       git clone $url $path
+
+      pushd $path
+      git checkout -b some_commit ${commit_id:1:-1}
+      popd
     fi
   done
 }
