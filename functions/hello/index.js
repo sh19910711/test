288b0ba049399ed9943c734d52c24de63def3c73
Hiroyuki S@no <hiroyuki.sano+git@mixi.co.jp>
2017-11-27 12:24:17 -0800

    copy from http://fartashh.github.io/post/serverless-approach-to-transcode-media/

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -1,5 +1,41 @@
-console.log('starting function')
-exports.handle = function(e, ctx, cb) {
-  console.log('processing event: %j', e)
-  cb(null, { hello: 'world' })
+'use strict';
+
+console.log('Loading function');
+
+let aws = require('aws-sdk');
+let s3 = new aws.S3({ apiVersion: '2006-03-01' });
+let elastictranscoder = new aws.ElasticTranscoder();
+
+function getFileName(path) {
+   return path.split('/').reverse()[0].split('.')[0];
 }
+
+exports.handler = (event, context, callback) => {
+
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+    var params = {
+      Input: { 
+        Key: key
+      },
+      PipelineId: '[USE YOUR PIPELINE ID]', 
+      OutputKeyPrefix: '[OUT PUT KEY PREFIX ex. `iphone/`]',
+      Outputs: [
+        {
+          SegmentDuration:'10.0',    
+          Key: getFileName(key),
+          PresetId: '[PRESETID ex `1351620000001-200060`]', // hls
+        }
+      ]
+    };
+
+    elastictranscoder.createJob(params, function(err, data) {
+      if (err){
+        console.log(err, err.stack); // an error occurred
+        context.fail();
+        return;
+      }
+      context.succeed();
+    });
+};
