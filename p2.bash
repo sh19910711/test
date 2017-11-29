de7dcaf85653633110dc7c398bcdcb74abb7ef55
Hiroyuki S@no <hiroyuki.sano+git@mixi.co.jp>
2017-11-29 14:29:51 -0800

    p2: Cattle
    Counter({u'Animal': 149, u'Mammal': 149, u'Cattle': 49, u'Cow': 45, u'Horse': 44, u'Wildlife': 35, u'Deer': 29, u'Sheep': 27, u'Camel': 11, u'Elk': 10, u'Antelope': 9, u'Impala': 7, u'Goat': 6, u'Person': 5, u'People': 5, u'Human': 5, u'Bull': 4, u'Aries': 3, u'Mountain Goat': 2, u'Nature': 2, u'Foal': 2, u'Herd': 2, u'Gazelle': 2, u'Potted Plant': 1, u'Jar': 1, u'Zoo': 1, u'Field': 1, u'Fog': 1, u'Tile': 1, u'Vase': 1, u'Plant': 1, u'Countryside': 1, u'Mosaic': 1, u'Pottery': 1, u'Flora': 1, u'Book': 1, u'Colt Horse': 1, u'Ox': 1, u'Grassland': 1, u'Art': 1, u'Meadow': 1, u'Fence': 1, u'Farm': 1, u'Weather': 1, u'Flock': 1, u'Grazing': 1})

--- p2.bash
+++ p2.bash
@@ -1,6 +1,51 @@
-export AWS_REGION=us-east-1
-
 create_collection() {
-  aws rekognition create-collection --region us-east-1 --collection-id animals
-  aws rekognition list-collections
+  aws rekognition --region us-east-1 create-collection --collection-id animals
+  aws rekognition --region us-east-1 list-collections
 }
+
+index_faces() {
+  for obj in $(aws s3 ls s3://analyticsjam/AI02/ | awk '{print $4}'); do
+    aws rekognition --region us-east-1 index-faces \
+      --image '{"S3Object":{"Bucket":"analyticsjam","Name":"AI02/'$obj'"}}' \
+      --collection-id animals \
+      --external-image-id $obj
+  done
+}
+
+sudo pip install var_dump
+cat <<PYTHON | python
+import boto3
+from var_dump import var_dump
+from collections import Counter
+
+def detect_labels(key):
+  rekognition = boto3.client("rekognition", "us-east-1")
+
+  response = rekognition.detect_labels(
+    Image={
+      "S3Object": {
+        "Bucket": "analyticsjam",
+        "Name": key,
+      }
+    },
+    MaxLabels=10,
+    MinConfidence=90
+  )
+  var_dump(response['Labels'])
+  return [l['Name'] for l in response['Labels']]
+
+def get_s3_keys():
+  s3 = boto3.client("s3", "us-east-1")
+  objs = s3.list_objects(
+    Bucket='analyticsjam',
+    Prefix='AI02/'
+  )
+  return [obj['Key'] for obj in objs['Contents']]
+
+c = Counter()
+for k in get_s3_keys():
+  print(k)
+  print("\n")
+  c += Counter(detect_labels(k))
+print(c)
+PYTHON
