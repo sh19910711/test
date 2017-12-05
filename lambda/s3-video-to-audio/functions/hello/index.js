216c95d34c5ac46b240af134c3ff15d99d95fb15
Hiroyuki S@no <hiroyuki.sano+git@mixi.co.jp>
2017-12-05 12:31:31 +0900

    lambda

--- lambda/s3-video-to-audio/functions/hello/index.js
+++ lambda/s3-video-to-audio/functions/hello/index.js
@@ -7,34 +7,33 @@ let s3 = new aws.S3({ apiVersion: '2006-03-01' });
 let elastictranscoder = new aws.ElasticTranscoder();
 
 function getFileName(path) {
-   return path.split('/').reverse()[0].split('.')[0];
+  return path.split('/').reverse()[0].split('.')[0];
 }
 
 exports.handle = (event, context, callback) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
 
-    const bucket = event.Records[0].s3.bucket.name;
-    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-
-    var params = {
-      Input: { 
-        Key: key
-      },
-      PipelineId: '1511814545326-jwbyte', 
-      OutputKeyPrefix: 'audios/',
-      Outputs: [
-        {
-          Key: getFileName(key) + ".mp3",
-          PresetId: '1351620000001-300010' // Audio MP3 - 320 k
-        }
-      ]
-    };
-
-    elastictranscoder.createJob(params, function(err, data) {
-      if (err){
-        console.log(err, err.stack); // an error occurred
-        context.fail();
-        return;
+  var params = {
+    Input: { 
+      Key: key
+    },
+    PipelineId: '1511814545326-jwbyte', 
+    OutputKeyPrefix: 'audios/',
+    Outputs: [
+      {
+        Key: getFileName(key) + ".mp3",
+        PresetId: '1351620000001-300010' // Audio MP3 - 320 k
       }
-      context.succeed();
-    });
+    ]
+  };
+
+  elastictranscoder.createJob(params, function(err, data) {
+    if (err){
+      console.log(err, err.stack); // an error occurred
+      context.fail();
+      return;
+    }
+    context.succeed();
+  });
 };
